b574deaf4485a2a2566745b7b2e954035165a6e2
Oracle & Web & Desktop Developer <90723420+Abdalazez-Azmi@users.noreply.github.com>
2022-12-09 17:50:33 +0300

    edit

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-let err= document.getElementById("err");
 let f_inp= document.getElementById("f_inp");
 let v_inp = f_inp.value;
-let url ="https://whatsapp://send?phone="
+let url ="https://wa.me://send?phone="
 let num ;
 
 
 const reffer=()=>{
     num = f_inp.value;
 if (f_inp.value.length === 10 ) {
-    err.innerText=""
     location.href=url+num
 
 }else{
