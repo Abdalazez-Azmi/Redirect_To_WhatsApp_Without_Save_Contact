4b1d33e04f9ff819e2de0fb0110a0ce10b276853
Oracle & Web & Desktop Developer <90723420+Abdalazez-Azmi@users.noreply.github.com>
2022-12-09 18:01:47 +0300

    edit

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 let f_inp= document.getElementById("f_inp");
 let v_inp = f_inp.value;
-let url ="https://wa.me://send?phone="
+let url ="https://api.whatsapp.com/send?phone=962"
 let num ;
 
 
 const reffer=()=>{
     num = f_inp.value;
-if (f_inp.value.length === 10 ) {
+if (f_inp.value.length <= 10 ) {
     location.href=url+num
 
 }else{
