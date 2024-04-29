d0299ed164082cfa71a32ed0135eba9025616eff
Oracle & Web & Desktop Developer <90723420+Abdalazez-Azmi@users.noreply.github.com>
2024-04-30 01:29:47 +0300

    edit

--- index.js
+++ index.js
@@ -6,11 +6,7 @@ let num  ;
 
 const reffer=()=>{
     if (f_inp.value.length === 10 && (f_inp.value[0] === "0"))  {
-        f_inp.value= f_inp.value.substring(1)
-        num = f_inp.value;
-    location.href=url+num
 
-}else{
    alert("Sorry The Number is not correct please re-check")
     console.log(f_inp.value.length)
 }
